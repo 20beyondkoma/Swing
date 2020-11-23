c50488983102a680933a4274011286c2f56b5e29
20beyondkoma <74659827+20beyondkoma@users.noreply.github.com>
2020-11-23 11:34:26 +0800

    Create 112301.App.java

---
+++ 112301.App.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("输入一个数，将为你输出随机坐标");
+
+
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            int user_inp = scanner.nextInt();
+            if (user_inp == 1) {
+                //方法一
+//                int x = (int) (Math.random() * 100 % 100);
+//                int y = (int) (Math.random() * 100 % 100);
+
+//                System.out.println("x随机坐标"+x);
+//                System.out.println("y随机坐标"+y);
+                //方法二
+                System.out.println("x随机坐标"+(int) (Math.random() * 100 % 100) +
+                  "\n"  + "y随机坐标"+(int) (Math.random() * 100 % 100));
+
+            }else {
+                System.out.println("错误代码，无法输入坐标");
+            }
+
+        }
+
+
+    }
+
+}
