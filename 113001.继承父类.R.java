6a34cdffa234cfaaecbd060afe876c0f69cee9e9
20beyondkoma <74659827+20beyondkoma@users.noreply.github.com>
2020-11-30 11:51:23 +0800

    继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,19 @@
+package pkg;
+
+public class R {
+	static void Lj(){
+//		int a=100;
+//		int b=1;
+//		 System.out.println("a*b的结果"+a*b);
+//		
+		
+		String boy_filePath="img/boy.gif";
+		String grill_filePath="img/grill.gif";
+		int [][]array2d= {{1,2,3},{4,5,6}}; 
+
+		System.out.println(boy_filePath+"\n"+grill_filePath);
+		System.out.println(array2d[0][1]);
+	 }
+ 
+ 
+}
