4006990b08689e19901e19d2f4aa6a17affb5051
20beyondkoma <74659827+20beyondkoma@users.noreply.github.com>
2020-12-08 14:14:13 +0800

    Create 120701.主类. App.java

---
+++ 120701.主类. App.java
@@ -0,0 +1,47 @@
+package pkg;
+
+public class App {
+
+	public static void main(String[] args) {
+//		
+//		int[][]array2d= {
+//				{1,2,3},
+//				{4,5,6},
+//				{7,8,0}
+//		};
+//		
+//		new Find().shuzu(array2d);
+//
+		
+		int[][]findZero= {
+				{1,2,3,4,5,6},
+				{7,8,9,10,0,12},
+				{7,8,9,10,11,12},
+				{7,8,9,10,11,12},
+				{88,66,92,102,151,999},
+		};
+		
+		
+		
+		new Find().shuzu(findZero);
+		new Find().shuzu2(findZero);
+    	new Find().shuzu3(findZero);
+    	new Find().shuzu4(findZero);
+    	new Find().shuzu5(findZero);
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+		
+		
+		
+		
+}
